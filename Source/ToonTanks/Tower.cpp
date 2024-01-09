274d5d5bb95175e722c1376c81cd09735569e8bc
youngil <youngil@placeb.com>
2024-01-09 09:35:05 +0900

    적 타워의 타이머 Fire

--- Source/ToonTanks/Tower.cpp
+++ Source/ToonTanks/Tower.cpp
@@ -4,25 +4,50 @@
 #include "Tower.h"
 #include "Tank.h"
 #include "Kismet/GameplayStatics.h"
+#include "TimerManager.h"
 
 void ATower::BeginPlay()
 {
 	Super::BeginPlay();
 
 	Tank = Cast<ATank>(UGameplayStatics::GetPlayerPawn(this, 0));
+
+	GetWorldTimerManager().SetTimer(
+		FireRateTimerHandle,	//타이머 핸들
+		this,					//유저 클래스
+		&ATower::CheckFireCondition,	//호출할 함수
+		FireRate,				//타이머 주기
+		true					//반복 여부
+	);
 }
 
 void ATower::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+	if (InFireRange())
+	{
+		RotateTurret(Tank->GetActorLocation());
+	}
+}
+
+void ATower::CheckFireCondition()
+{
+	if (InFireRange())
+	{
+		Fire();
+	}
+}
+
+bool ATower::InFireRange() const
+{
 	if (Tank)
 	{
 		float Distance = FVector::Dist(GetActorLocation(), Tank->GetActorLocation());
 		if (Distance <= FireRange)
 		{
-			RotateTurret(Tank->GetActorLocation());
+			return true;
 		}
 	}
-
+	return false;
 }
